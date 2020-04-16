2375b6ddb9165844cd3ee27c452fe4a98478f680
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-04-16 16:59:22 +0100

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,7 +1,15 @@
 function openForm() {
-    document.getElementById("loginForm").style.display = "block";
+  document.getElementById("loginForm").style.display = "block";
   }
   
-  function closeForm() {
-    document.getElementById("loginForm").style.display = "none";
+function closeForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
+
+function abrirForm() {
+  document.getElementById("tarefaForm").style.display = "block";
   }
+  
+function fecharForm() {
+  document.getElementById("tarefaForm").style.display = "none";
+}
